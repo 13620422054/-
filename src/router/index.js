d62af557fc38095f15f9a3276c5f233b36abb8ae
aa <12.com>
2018-03-08 20:06:34 +0800

    校验登陆

--- src/router/index.js
+++ src/router/index.js
@@ -33,9 +33,58 @@ let order = [
     { name: "OrderComplete", path: '/order/complete', component: OrderComplete }
 ]
 
-export default new Router({
+
+
+let router = new Router({
     routes: [
         { name: 'Login', path: '/login', component: Login },
         { name: 'Shop', path: '/', component: Shop, children: [...goods, ...shopcart, ...order] }
     ]
-})
+});
+
+// 网站白名单
+let baidan = ['GoodsList', 'GoodsDetail', 'Shopcart'];
+
+
+router.beforeEach((to, from, next) => {
+    // 判断当前页在不在白名单中
+    let isTrue = baidan.some(v => v == to.name);
+    // 如果是白名单.不用登陆
+    if (isTrue) {
+        next();
+        return;
+    }
+
+    // 现在是js文件,不是组件,不能直接使用this.$http.
+    Vue.prototype.$http.get(Vue.prototype.$api.isLogin).then((res) => {
+        // 让登录状态默认为未登录
+        let islogin = false;
+
+        if (res.data.code == "logined") {
+            islogin = true;
+        }
+        // 如果是访问登录页面
+        if (to.name == 'Login') {
+            // 判断是否登录
+            //如果登录了.自动跳转到后面页面
+            if (islogin) {
+                next({ name: 'goods/list' })
+            } else {
+                // 未登录,允许访问
+                next();
+            }
+        }
+
+        //如果访问登录页面以外的后台页面
+        if (to.name != 'Login') {
+            //判断是否访问
+            if (!islogin) {
+                //如果未登录
+                next({ name: 'Login', query: { next: to.fullPath } })
+            } else {
+                next();
+            }
+        }
+    })
+});
+export default router;
